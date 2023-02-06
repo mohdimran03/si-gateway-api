c7cf23788c3c33d4d094e1b47deb879237d400ba
mohd_imran <mohd.imran@topica.edu.vn>
2023-02-06 21:36:30 +0530

    typescript configuration

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+let app = express();
+app.listen(3001, () => {
+    console.log('server is running');
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
+import express from 'express'
+
+// const gateway = new ApolloGateway({
+//     supergraphSdl: new IntrospectAndCompose({
+//       subgraphs: [
+//           // List of federation-capable GraphQL endpoints...
+//          //{ name: "user-service", url: "http://localhost:4001/graphql" }
+//       ],
+//     }),
+//   });
+  
+//   const server = new ApolloServer({ gateway });
+  
+//   // Note the top-level await!
+//   const { url } = await startStandaloneServer(server);
+//   console.log(`🚀  Server ready at ${url}`);
+  
+let app = express();
+
+app.listen(3001,()=> {
+  console.log('server is running')
+})
